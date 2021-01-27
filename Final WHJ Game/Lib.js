15abaf5525ae9938bfd66de1d371dbddc1f48744
Starranken <72114247+Starranken@users.noreply.github.com>
2021-01-26 16:51:18 -0800

    Delete Final WHJ Game directory

--- Final WHJ Game/Lib.js
+++
@@ -1,8 +0,0 @@
-function bounceOff(object1, object2){
-    if(object1.body.position.x-object2.body.position.x<=object1.w/2+object2.width/2
-        &&object2.body.position.x-object1.body.position.x<=object2.width/2+object1.w/2
-        &&object1.body.position.y-object2.body.position.y<=object1.h/2+object2.height/2
-        &&object2.body.position.y-object1.body.position.y<=object2.height/2+object1.h/2){
-        Matter.Body.setVelocity(object1.body,{x:-2, y:0}) 
-    }
-}
